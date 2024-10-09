a7fb7aaf7519cd5c5467e2bd1b86789cda13a101
hanumanKalhale2023 <132592044+hanumanKalhale2023@users.noreply.github.com>
2024-10-09 09:02:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+gsap.to(".sun",{
+    rotation:360,
+    repeat:-1,
+    duration:9,
+})
+gsap.to(".mercury",{
+    radius:390,
+    duration:1,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-80px -0px 39px`
+})
+gsap.to(".venus",{
+    radius:900,
+    duration:2,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-60px -60px`
+})
+gsap.to(".earth",{
+    radius:900,
+    duration:3,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-80px -80px`
+})
+gsap.to(".mars",{
+    radius:900,
+    duration:4,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-130px -130px`,
+})
+gsap.to(".jupiter",{
+    radius:600,
+    duration:5,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-160px -160px`,
+})
+gsap.to(".satern",{
+    radius:600,
+    duration:7,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-190px -160px`,
+})
+gsap.to(".uranus",{
+    radius:600,
+    duration:8,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-200px -200px`,
+})
+gsap.to(".neptune",{
+    radius:600,
+    duration:9,
+    rotation:360,
+    repeat:-1,
+    transformOrigin:`-230px -230px`,
+})
+
